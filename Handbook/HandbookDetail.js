3e7b2aaee6e5d0c8fdc0019fcaa090d9e5ce3045
Aaron <luanjiwu5@163.com>
2018-05-15 17:28:21 +0800

    隐藏属性

--- Handbook/HandbookDetail.js
+++ Handbook/HandbookDetail.js
@@ -9,7 +9,13 @@ import {TextTouchableCell} from './DetailCells/TextTouchableCell'
 export class HandbookDetail extends React.Component{
 
   showAbilityDetail = () => {
-    console.log('show ability');
+    const pokemon = this.props.navigation.state.params.pokemon;
+    console.log('show ability: '+pokemon.ability);
+  };
+
+  showHiddenAbilityDetail = () => {
+    const pokemon = this.props.navigation.state.params.pokemon;
+    console.log('show hidden ability: '+pokemon.hiddenAbility);
   };
 
   render () {
@@ -19,13 +25,17 @@ export class HandbookDetail extends React.Component{
     var detailsHandlers = {};
     detailsHandlers[ability] = this.showAbilityDetail.bind(this);
 
+    var hiddenAbility =  I18n.t(pokemon.hidden_ability)
+    var hiddenDetailsHandlers = {};
+    hiddenDetailsHandlers[hiddenAbility] = this.showHiddenAbilityDetail.bind(this);
+
     return (
       <ScrollView style={styles.container}>
         <Image source={{uri: pokemon.image}} style={{width: 180, height: 180}} />
         <TextCell title={I18n.t(pokemon.name)} detail={pokemon.id} />
         <TypeCell types={pokemon.type} />
         <TextTouchableCell title={I18n.t('ability')} details={detailsHandlers} />
-        {/* <TextTouchableCell title={I18n.t('hidden_ability')} details={[I18n.t(pokemon.hidden_ability)]} /> */}
+        <TextTouchableCell title={I18n.t('hidden_ability')} details={hiddenDetailsHandlers} />
       </ScrollView>
     );
   }
