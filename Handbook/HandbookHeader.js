39c3a1635dd4a320dd6798d4828c6f1a749315a2
Aaron <luanjiwu5@163.com>
2018-05-17 14:27:02 +0800

    样式调整

--- Handbook/HandbookHeader.js
+++ Handbook/HandbookHeader.js
@@ -7,7 +7,7 @@ export class HandbookHeader extends React.Component {
     console.log('title: '+ title);
     return (
       <View style={{height: 50, backgroundColor: 'white', justifyContent:'center'}}>
-        <Text style={{left: 15, color:'353535', fontSize:19, fontWeight:'normal'}}>{title}</Text>
+        <Text style={{left: 15, color:'#353535', fontSize:19, fontWeight:'normal'}}>{title}</Text>
       </View>
     );
   }
--- Handbook/TypesView.js
+++ Handbook/TypesView.js
@@ -6,11 +6,13 @@ class TypeView extends React.Component {
     render () {
       let type = this.props.type;
       let name = I18n.t(type);
-      let style = styles[type];
-
+      let typeStyle = styles[type];
+      let textBGStyle = [typeStyle, styles.textBG];
       return (
-        <View style={style}>
-          <Text style={styles.text}>{name}</Text>
+        <View style={styles.container}>
+          <View style={textBGStyle}>
+            <Text style={styles.text}>{name}</Text>
+          </View>
         </View>
       );
     }
@@ -29,14 +31,41 @@ export class TypesView extends React.Component {
   }
 }
 
+// display: inline-block;
+//     box-sizing: border-box;
+//     box-shadow: 0 0 1px 1px #fff inset;
+//     line-height: 16px;
+
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems:'center',
+    borderRadius: 5,
+    borderWidth: 0.5,
+    borderColor:'#656557',
+    borderStyle:'solid',
+    backgroundColor: 'white',
+    width: 72,
+    height: 20,
+  },
+  textBG: {
+    justifyContent: 'center',
+    borderRadius: 5,
+    borderWidth: 0,
+    width: 69,
+    height: 17,
+  },
   text: {
-    fontSize: 14,
+    fontSize: 13,
     fontWeight: 'bold',
     color: 'white',
+    textShadowOffset:{width:0.5, height:0.5},
+    textShadowColor:'#656565',
+    textShadowRadius:2,
+    textAlign: 'center',
   },
   grass: {
-    backgroundColor: 'green',
+    backgroundColor: '#77CC55',
   },
   poison: {
     backgroundColor: 'purple',
