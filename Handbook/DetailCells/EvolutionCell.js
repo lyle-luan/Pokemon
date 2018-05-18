7208a6687db1a873e8d3d8d568b4e1cc0c8124ab
Aaron <luanjiwu5@163.com>
2018-05-18 10:31:38 +0800

    进化 view 样式修正

--- Handbook/DetailCells/EvolutionCell.js
+++ Handbook/DetailCells/EvolutionCell.js
@@ -3,6 +3,7 @@ import {StyleSheet, Text, View, Image} from 'react-native';
 import I18n from '../../i18n/i18n';
 import Evolution from '../../PokemonData/Evolution.json';
 import Pokemons from '../../PokemonData/Pokemons.json';
+import {HandbookListItem} from '../HandbookListItem';
 
 export class EvolutionCell extends React.Component {
   render () {
@@ -11,19 +12,26 @@ export class EvolutionCell extends React.Component {
 
     let pokemon = Pokemons[pokemonID];
 
+    console.log('====>pokemonID:'+ pokemonID);
+
     return (
       <View style={styles.container}>
-        <Image source={{uri: pokemon.image}} style={{width: 180, height: 180}} />
-        <Text>{I18n.t(pokemon.name)}</Text>
-
+        <HandbookListItem pokemonID={pokemonID} />
+        <View style={{flex:1, flexDirection: 'row',}}>
+          <View style={{width:90, height:1, backgroundColor: 'white'}}></View>
+          <View style={{flex:1, height:1, backgroundColor: '#E9E9E9'}}></View>
+        </View>
         {
           evolutionAarry.map((evolution)=>(
             <View style={{flex:1, flexDirection: 'column'}}>
               <Text>{I18n.t('level')+evolution.level}</Text>
               <Text>{evolution.carry_item}</Text>
               <Text>{evolution.condition}</Text>
-              <Image source={{uri: Pokemons[evolution.pokemon_id].image}} style={{width: 180, height: 180}} />
-              <Text>{I18n.t(Pokemons[evolution.pokemon_id].name)}</Text>
+              <HandbookListItem pokemonID={evolution.pokemon_id} />
+              <View style={{flex:1, flexDirection: 'row',}}>
+                <View style={{width:90, height:1, backgroundColor: 'white'}}></View>
+                <View style={{flex:1, height:1, backgroundColor: '#E9E9E9'}}></View>
+              </View>
             </View>
           ))
         }
